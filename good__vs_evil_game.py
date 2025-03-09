0f97a736f6410ddab010b223d29a98bf135a718f
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-09 22:07:57 +0800

    User selects good or evil. Also imported random for fate

--- good__vs_evil_game.py
+++ good__vs_evil_game.py
@@ -1,3 +1,5 @@
+import random
+
 # Ask for the users name, greets the user
 name = input("What is your name new student? \n")
 gender = int(input("Are you a boy or a girl? \n1. Boy \n2. Girl \n(Pick 1 or 2): "))
@@ -8,5 +10,16 @@ elif gender == 2:
 print(f"Welcome to the school for good and evil {prefix} {name}")
 # Show intro about the world
 # Select wether good or evil
+status = int(input("Are you good or evil? \n1. Good \n2. Evil \n3. I don't know, let fate decide \n(Pick 1-3) \n"))
+if status == 1:
+    print(f"Welcome to the school for good {prefix} {name}.")
+elif status == 2:
+    print(f"Welcome to the school for evil {prefix} {name}.")
+elif status == 3:
+    status = random.randint(1, 2)
+    if status == 1:
+        print(f"Welcome to the school for good {prefix} {name}.")
+    elif status == 2:
+        print(f"Welcome to the school for evil {prefix} {name}.")
 # Select talent/traits
 # Think of further features
