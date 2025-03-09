1515ab7310b06f97b7865be2968d268ec425f76a
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-09 22:22:41 +0800

    Add pseudocode for traits/talents. And fixed user input to string rather than integers

--- good__vs_evil_game.py
+++ good__vs_evil_game.py
@@ -1,25 +1,29 @@
 import random
 
 # Ask for the users name, greets the user
-name = input("What is your name new student? \n")
-gender = int(input("Are you a boy or a girl? \n1. Boy \n2. Girl \n(Pick 1 or 2): "))
-if gender == 1:
+name = input("What is your name, new student? \n")
+gender = input("Are you a boy or a girl? \n(Type 'girl' or 'boy'): \n").strip().lower()
+if gender == "boy":
     prefix = "Mr."
-elif gender == 2:
+elif gender == "girl":
     prefix = "Ms."    
 print(f"Welcome to the school for good and evil {prefix} {name}")
 # Show intro about the world
 # Select wether good or evil
-status = int(input("Are you good or evil? \n1. Good \n2. Evil \n3. I don't know, let fate decide \n(Pick 1-3) \n"))
-if status == 1:
+status = input("Are you good or evil? Or would you rather let fate decide? \n(Type 'good', 'evil', or 'fate'): \n").strip().lower()
+if status == "good":
     print(f"Welcome to the school for good {prefix} {name}.")
-elif status == 2:
+elif status == "evil":
     print(f"Welcome to the school for evil {prefix} {name}.")
-elif status == 3:
+elif status == "fate":
     status = random.randint(1, 2)
     if status == 1:
+        status = "good"
         print(f"Welcome to the school for good {prefix} {name}.")
     elif status == 2:
+        status = "evil"
         print(f"Welcome to the school for evil {prefix} {name}.")
 # Select talent/traits
+    # Talents for good: Animal communication, sword talent, charisma, magic adept
+    # Traits for evil: Beast communication, potion master, charisma, curse adept
 # Think of further features
