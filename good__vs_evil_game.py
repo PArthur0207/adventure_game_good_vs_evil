c21f68d86228bb3113a259af882d14c7254eb2ec
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-09 21:38:52 +0800

    Asks for User name and prefix. And greet the user

--- good__vs_evil_game.py
+++ good__vs_evil_game.py
@@ -1,4 +1,11 @@
 # Ask for the users name, greets the user
+name = input("What is your name new student? \n")
+gender = int(input("Are you a boy or a girl? \n1. Boy \n2. Girl \n(Pick 1 or 2): "))
+if gender == 1:
+    prefix = "Mr."
+elif gender == 2:
+    prefix = "Ms."    
+print(f"Welcome to the school for good and evil {prefix} {name}")
 # Show intro about the world
 # Select wether good or evil
 # Select talent/traits
