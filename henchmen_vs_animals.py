25452a77600a74a0fda800f933baf56b93df8743
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-18 22:49:05 +0800

    Fixed the names and asks the user to pick their choice (asks until valid)

--- henchmen_vs_animals.py
+++ henchmen_vs_animals.py
@@ -2,12 +2,24 @@
 henchmen = {
     # All stats when combined must be equal to 300 for balance
     "Ogre": {"health": 120, "strength": 150, "speed": 30}, # Special talent: Has a chance to reduce the opponent's damage significantly
-    "Goblins": {"health": 80, "strength": 80, "speed": 140}, # Special talent: Can multiply (Limit this)
+    "Goblin": {"health": 80, "strength": 80, "speed": 140}, # Special talent: Can multiply (Limit this)
     "Banshee": {"health": 100, "strength": 80, "speed": 120}, # Special talent: Is hard to hit 
-    "Werewolves": {"health": 100, "strength": 100, "speed": 100}, # Special talent: Has a chance to increase all stats when full moon
+    "Werewolve": {"health": 100, "strength": 100, "speed": 100}, # Special talent: Has a chance to increase all stats when full moon
     # Add more henchmens in the future
 }
+
 # Asks user to pick which henchmen to pick
+while True:
+    print("Pick your henchman:", ", " .join(henchmen.keys()))
+    henchman_choice = input("Enter your henchman's name: ").strip().title()
+
+    if henchman_choice in henchmen:
+        ally = henchmen[henchman_choice]
+        print(henchman_choice, ally)
+        break
+
+    else:
+        ("Invalid choice, please pick only from the choices.")
 # Do a turn based battle that users can have fun with
 # Possibly add moves by user that is not among the movesets of the henchmen (do this later)
 # Do the same for animal allies (for good)
