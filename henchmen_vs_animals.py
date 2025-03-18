5352dd747abf7eeaa8bee6913a5b280c66e7f4d2
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-18 21:39:33 +0800

    Pseudocode

---
+++ henchmen_vs_animals.py
@@ -0,0 +1,5 @@
+# Initialize list of henchmens, complete with health, movesets, and special talents
+# Asks user to pick which henchmen to pick
+# Do a turn based battle that users can have fun with
+# Possibly add moves by user that is not among the movesets of the henchmen (do this later)
+# Do the same for animal allies (for good)
